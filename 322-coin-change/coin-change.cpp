847dd96cc3fd6af35b20fbe6b0d1c05a7d08c203
Debajyoti Chakraborty <debajyotinit8@gmail.com>
2026-08-06 00:21:38 +0530

    Time: 39 ms (48.78%) | Memory: 44.4 MB (46.92%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,36 +2,39 @@ class Solution {
 public:
 
     int coinChange(vector<int>& coins, int amount) {
+        //Space
         // Time Complexity: O(n × amount)
-        // Space Complexity: O(n × amount)
+        // Space Complexity: O( amount)
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<int> prev(amount+1,0);
 
         //base cases for index 0
         for(int t=0;t<=amount;t++)
         {
             if(t%coins[0]==0)
-            dp[0][t]=t/coins[0];
+            prev[t]=t/coins[0];
             else 
-            dp[0][t]=1e9;
+            prev[t]=1e9;
         }
 
         //change parameters
         for(int i=1;i<n;i++)
         {
+           vector<int> curr(amount+1,0);
            for(int t=0;t<=amount;t++)
            {
                 //copy recursion code
                 int pick=1e9;
                 if(coins[i]<=t)
-                pick=1+ dp[i][t-coins[i]];
-                int notpick=dp[i-1][t];
+                pick=1+ curr[t-coins[i]];
+                int notpick=prev[t];
 
-                dp[i][t]=min(pick,notpick);
+                curr[t]=min(pick,notpick);
            }
+           prev=curr;
         }
 
-        int ans = dp[n-1][amount];
+        int ans = prev[amount];
 
 
         if(ans >= 1e9)
