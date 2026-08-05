d03c9c9774db07d7cfc546773573c3a06156ea3e
Debajyoti Chakraborty <debajyotinit8@gmail.com>
2026-08-06 00:16:33 +0530

    Time: 55 ms (18.68%) | Memory: 48.9 MB (37.71%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
 
-    int f(int i,vector<int>& coins, int amount,vector<vector<int>> &dp)
-    {
-        
-        if(i==0)
-        {
-            if(amount%coins[i]==0)
-            return (amount/coins[i]);
-            else 
-            return 1e9;
-        }
-        if(dp[i][amount]!=-1)
-        return dp[i][amount];
-
-
-        int pick=1e9;
-        if(coins[i]<=amount)
-        pick=1+f(i,coins,amount-coins[i],dp);
-        int notpick=f(i-1,coins,amount,dp);
-
-        return dp[i][amount]=min(pick,notpick);
-    }
-
     int coinChange(vector<int>& coins, int amount) {
         // Time Complexity: O(n × amount)
-        // Space Complexity: O(n × amount) + O(amount)
+        // Space Complexity: O(n × amount)
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans = f(n-1, coins, amount,dp);
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+
+        //base cases for index 0
+        for(int t=0;t<=amount;t++)
+        {
+            if(t%coins[0]==0)
+            dp[0][t]=t/coins[0];
+            else 
+            dp[0][t]=1e9;
+        }
+
+        //change parameters
+        for(int i=1;i<n;i++)
+        {
+           for(int t=0;t<=amount;t++)
+           {
+                //copy recursion code
+                int pick=1e9;
+                if(coins[i]<=t)
+                pick=1+ dp[i][t-coins[i]];
+                int notpick=dp[i-1][t];
+
+                dp[i][t]=min(pick,notpick);
+           }
+        }
+
+        int ans = dp[n-1][amount];
 
 
         if(ans >= 1e9)
