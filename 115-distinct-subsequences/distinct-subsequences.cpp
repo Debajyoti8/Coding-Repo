105e710accb89abc411b42c55c70a1001b76023f
Debajyoti Chakraborty <debajyotinit8@gmail.com>
2026-08-16 02:05:35 +0530

    Time: 7 ms (96.20%) | Memory: 8.8 MB (96.02%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -2,32 +2,29 @@ class Solution {
 public:
 
     int numDistinct(string s, string t) {
-        //Tabulation 1-based idx to avoid neg indexing
+        //Space opt 1-based idx to avoid neg indexing
+        //using 1 1D array
         //tc-O(nm)
-        //sc-O(nm)
+        //sc-O(m)
         int n=s.size();
         int m=t.size();
-        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+        vector<double> prev(m+1,0);
 
         //base cases
-        for(int i=0;i<=n;i++)
-        dp[i][0]=1; 
+        prev[0]=1;  //0th idx elem will always be 1
         //2nd base case already covered due to initialising with 0
         
         //use nested loops 
         for(int i=1;i<=n;i++) //i=0 base case already covered
         {
-            for(int j=1;j<=m;j++)
+            for(int j=m;j>=1;j--)
             {
                 //explore all possibilities               
                     if(s[i-1]==t[j-1])
-                    dp[i][j]= dp[i-1][j-1]+dp[i-1][j]; 
-                    else 
-                    dp[i][j]=dp[i-1][j]; 
-                
+                    prev[j]= prev[j-1]+prev[j];                               
             }
         }
 
-        return (int)dp[n][m];
+        return (int)prev[m]; //typecasted to int
     }
 };
