ff5dda6cdc77c23db28d5826dd1b1607d4a7e4e7
Debajyoti Chakraborty <debajyotinit8@gmail.com>
2026-08-21 23:52:22 +0530

    Time: 18 ms (5.17%) | Memory: 27.9 MB (8.27%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
+
+    int f(int i,int n,vector<int>& prices,int buy,vector<vector<int>> &dp)
+    {
+        //base case
+        if(i==n) //Agar saare days khatam ho gaye:toh ab profit kama hi nahi sakte.
+        return 0;
+        if(dp[i][buy]!=-1) return dp[i][buy];
+
+        int profit=0;
+        if(buy) //if not buy till  now,just buy it
+        profit=max(-prices[i] + f(i+1,n,prices,0,dp),f(i+1,n,prices,1,dp)); //max(buy, skip)
+        else    //sell
+        profit=max(prices[i] + f(i+1,n,prices,1,dp), f(i+1,n,prices,0,dp));
+
+        return dp[i][buy]=profit;
+    }
+
     int maxProfit(vector<int>& prices) {
-        int profit=0,n=prices.size();
-        int mini=prices[0];  //stores min buy price
-
-        for(int i=1;i<n;i++)
-        {
-            int cost=prices[i]-mini;
-            if(cost>0) 
-            {
-                profit+=cost;
-                mini=prices[i]; 
-            }
-            else
-            mini=min(mini,prices[i]);    
-        }
-
-        return profit;
+        //Memoization code
+        //Time - O(n)
+        //Space - O(n) + O(n)
+        int n=prices.size();
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        return f(0,n,prices,1,dp);
     }
 };
