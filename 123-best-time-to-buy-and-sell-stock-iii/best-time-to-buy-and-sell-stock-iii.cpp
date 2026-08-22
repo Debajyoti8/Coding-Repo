7d710c50ddc5cbc46191a4f1ae9960c2ef15380b
Debajyoti Chakraborty <debajyotinit8@gmail.com>
2026-08-22 21:45:53 +0530

    Time: 143 ms (60.47%) | Memory: 120.9 MB (62.24%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,31 +3,33 @@ public:
     int maxProfit(vector<int>& prices) {
         //Striver 
         //2nd method
-        //Tabulaton code
+        //Space opt code
         // Time-O(n×4)
-        // Space-O(n×4) 
+        // Space-O(4) 
         int n=prices.size();
 
-        //2d dp of (ind,buy,cap)
-        vector<vector<int>> dp(n+1,vector<int>(5,0));
+        //1d dp 
+        vector<int> after(5,0);
 
         //base case already covered (i==n t==4)
 
         //explore
         for(int i=n-1;i>=0;i--) //loop starts in opp. fashion
         {
+            vector<int> curr(5,0);
             for(int t=3;t>=0;t--)  //loop starts in opp. fashion
             {
                 //copy recurrence
                     if(t%2==0) //if not buy till  now,just buy it
-                        dp[i][t]=max(-prices[i] + dp[i+1][t+1],dp[i+1][t]); //max(buy, skip)
+                        curr[t]=max(-prices[i] + after[t+1],after[t]); //max(buy, skip)
                     else    //sell 
-                        dp[i][t]=max(prices[i]+dp[i+1][t+1],dp[i+1][t]);
+                        curr[t]=max(prices[i]+after[t+1],after[t]);
                 
             }
+            after=curr;
         }
 
         //return will always be initial call made in recursion
-        return dp[0][0];
+        return after[0];
     }
 };
