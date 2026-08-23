e3d60ae9092f3412a2bfe69d6981c091697221b5
Debajyoti Chakraborty <debajyotinit8@gmail.com>
2026-08-23 20:10:40 +0530

    Time: 98 ms (61.76%) | Memory: 87.8 MB (64.50%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
 
     int maxProfit(vector<int>& prices, int fee) {
-        //Tabulation code
+        //Space opt code
         //Time - O(n) 
-        //Space- O(n) 
+        //Space- O(2+2)=O(1) 
         int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        vector<int> front(2,0);
 
         //base acse covered
         for(int i=n-1;i>=0;i--)
         {
-             //more optimised tabulation remove buy
-                //copy recursion                
-                dp[i][1]=max(-prices[i] + dp[i+1][0] ,dp[i+1][1]); //max(buy, skip)
+             vector<int> curr(2,0);
+                curr[1]=max(-prices[i] + front[0] ,front[1]); //max(buy, skip)
                 //if sell cooldown occurs /skip
-                dp[i][0]=max(prices[i]-fee + dp[i+1][1] , dp[i+1][0]);                
+                curr[0]=max(prices[i]-fee + front[1] , front[0]);       
+
+                front =curr;         
         }
 
-        return dp[0][1];
+        return front[1];
     }
 };
