cbeca6703b2a37c8f7ec0285a3aaf18fa2a9c678
Debajyoti Chakraborty <debajyotinit8@gmail.com>
2026-08-23 18:59:58 +0530

    Time: 4 ms (18.22%) | Memory: 16.1 MB (52.94%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int f(int i,int n,vector<int>& prices,int buy,vector<vector<int>> &dp)
-    {
-        //base case
-        if(i>=n) //Agar saare days khatam ho gaye:toh ab profit kama hi nahi sakte.
-        return 0;
-        if(dp[i][buy]!=-1) return dp[i][buy];
-
-        int profit=0;
-        if(buy) //if not buy till  now,just buy it
-        profit=max(-prices[i] + f(i+1,n,prices,0,dp),f(i+1,n,prices,1,dp)); //max(buy, skip)
-        else    //if sell cooldown occurs /skip
-        profit=max(prices[i] + f(i+2,n,prices,1,dp), f(i+1,n,prices,0,dp));
-
-        return dp[i][buy]=profit;
-    }
 
     int maxProfit(vector<int>& prices) {
-        //Memoiztaion
+        //Tabulation
         // Time: n × 2 = O(n)
-        // Space: O(2n) = O(n)+O(n)  [f(0) → f(1) → ... → f(n)] Rec stack
+        // Space: O(2n) = O(n)
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return f(0,n,prices,1,dp);
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+
+        //base case already covererd
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy=0;buy<2;buy++)
+            {
+                //copy recursion
+                int profit=0;
+                if(buy) //if not buy till  now,just buy it
+                profit=max(-prices[i] + dp[i+1][0] ,dp[i+1][1]); //max(buy, skip)
+                else    //if sell cooldown occurs /skip
+                profit=max(prices[i] + dp[i+2][1] , dp[i+1][0]);
+
+                dp[i][buy]=profit;
+            }
+        }
+
+        return dp[0][1];
     }
 };
