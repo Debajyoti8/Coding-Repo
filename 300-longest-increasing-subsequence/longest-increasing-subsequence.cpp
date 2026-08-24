a0f58389ba00494105f0a094d3d5ed5299910841
Debajyoti Chakraborty <debajyotinit8@gmail.com>
2026-08-24 10:17:24 +0530

    Time: 276 ms (33.62%) | Memory: 296.5 MB (13.65%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int f(int i,int prev,int n,vector<int>& nums,vector<vector<int>> &dp)
-    {
-        if(i==n) return 0;
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-
-        //max(pick,not pick)        
-        //Not pick
-        int len=f(i+1,prev,n,nums,dp);
-        //pick
-        if(prev==-1||nums[i]>nums[prev]) 
-        len=max(len,1 + f(i+1,i,n,nums,dp));
-
-        return dp[i][prev+1]=len;
-    }
 
     int lengthOfLIS(vector<int>& nums) {
-        //Memoization
+        //Tabulation
         //Time  : O(n^2)
-        // Space :O(n^2)+ O(n)
+        // Space :O(n^2)
         int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1)); //dp mai 1 prev_idx shift krke store krna hai 
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0)); //dp mai 1 prev_idx shift krke store krna hai 
 
-        return f(0,-1,n,nums,dp);
+        // base case already covered
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int prev=i-1;prev>=-1;prev--)
+            {
+                //copy recursion
+                //max(pick,not pick)        
+                //Not pick
+                int len=dp[i+1][prev+1];   //co-ordinate shift has to be done
+                //pick
+                if(prev==-1||nums[i]>nums[prev]) 
+                len=max(len,1 + dp[i+1][i+1]);
+
+                dp[i][prev+1]=len;
+            }
+        }
+
+        return dp[0][-1+1] ; //co-ordinate shift has to be done
     }
 };
