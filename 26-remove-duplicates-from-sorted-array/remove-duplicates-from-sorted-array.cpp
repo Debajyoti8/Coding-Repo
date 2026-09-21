e628f9ece0ca79a179579c4721329bb972e4df07
Debajyoti Chakraborty <debajyotinit8@gmail.com>
2026-09-21 20:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (80.32%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n=nums.size();
-        int i=0;
-        for(int j=1;j<n;j++)
-        {
-            if(nums[j]!=nums[i])
-            {
-                nums[i+1]=nums[j];
+        // TC: O(n)
+        // SC: O(1)
+
+        int n = nums.size();
+        int i = 0;  // i = last index of unique elements
+
+        for(int j = 1; j < n; j++) {
+            // If a new unique element is found
+            if(nums[j] != nums[i]) {
+                nums[i + 1] = nums[j];  // Place it after unique portion
                 i++;
             }
         }
 
-        return i+1;
+        // First i+1 elements contain all unique elements
+        return i + 1;
     }
 };
